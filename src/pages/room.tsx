2e07d1ed4de2f7c8dc88391573d52011c69dab97
fabiobatoni <fabio_batoni@hotmail.com>
2024-08-08 13:18:38 -0300

    create http method create-message

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom"
 import amaLogo from '../assets/ama-logo.svg'
-import { ArrowRight, Share2 } from "lucide-react"
+import { Share2 } from "lucide-react"
 import { toast } from "sonner"
 import { Messages } from "../components/messages"
 import { Suspense } from "react"
+import { CreateMessageForm } from "../components/create-message-form"
 
 export function Room() {
 
@@ -42,25 +43,7 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form 
-        className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
-      >
-        <input 
-          type="text" 
-          name="theme"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-        />
-
-        <button 
-          type="submit" 
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Criar pergunta
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm />
 
       <Suspense fallback={<p>Carregando ...</p>}>
         <Messages />
